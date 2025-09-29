b1b8b7eab66a7465b265e21bc8f02e801f3c1693
Helgard Ferreira <hrfer3@gmail.com>
2025-09-29 15:46:30 +0200

    feat: implement basic start and stop buttons

--- src/app.tsx
+++ src/app.tsx
@@ -1,53 +1,145 @@
-import { createSignal } from 'solid-js';
+import { timeFormat } from 'd3';
+import {
+  type Component,
+  type ComponentProps,
+  Show,
+  batch,
+  createEffect,
+  createMemo,
+  createSignal,
+  onCleanup,
+  splitProps,
+} from 'solid-js';
 
-import solidLogo from './assets/solid.svg';
-import viteLogo from '/vite.svg';
+import { cn } from './utils';
 
-function App() {
-  const [count, setCount] = createSignal(0);
+// TODO: move this
+//// ---------------------------------------------------------------------------
+export type ButtonProps = ComponentProps<'button'>;
+
+// TODO: implement variants via `class-variance-authority`
+// TODO: continue here...
+export const Button: Component<ButtonProps> = (props) => {
+  const [local, rest] = splitProps(props, ['class']);
+
+  // TODO: implement styling
+  return (
+    <button
+      class={cn(
+        'bg-slate-950',
+        'border',
+        'cursor-pointer',
+        'focus-visible:outline-2',
+        'hover:bg-slate-800',
+        'min-w-38',
+        'outline-offset-1',
+        'rounded-full',
+        'text-white',
+        local.class
+      )}
+      {...rest}
+    />
+  );
+};
+//// ---------------------------------------------------------------------------
+
+// TODO: extract logic into separate hook(s)
+// TODO: implement the following buttons:
+//       - Lap
+//        - Lap should also be the first "left" button but in a disabled state at first
+//       - Stop
+//       - Reset
+//       - Start
+// TODO: continue here...
+const App: Component = () => {
+  const [previousElapsedMs, setPreviousElapsedMs] = createSignal(0);
+  const [elapsedMs, setElapsedMs] = createSignal(0);
+  const [isActive, setIsActive] = createSignal(false);
+
+  // TODO: implement ms time format similar to Apple "Clock" App
+  const timeFormatter = timeFormat('%H:%M:%S.%L');
+
+  const referenceDate = new Date();
+  referenceDate.setHours(0);
+  referenceDate.setMinutes(0);
+  referenceDate.setSeconds(0);
+  referenceDate.setMilliseconds(0);
+
+  const elapsedDate = new Date();
+  elapsedDate.setHours(0);
+  elapsedDate.setMinutes(0);
+  elapsedDate.setSeconds(0);
+  elapsedDate.setMilliseconds(0);
+
+  // TODO: implement this
+  const formattedElapsedTime = createMemo(() => {
+    elapsedDate.setTime(referenceDate.getTime() + elapsedMs());
+
+    return timeFormatter(elapsedDate);
+  });
+
+  createEffect(() => {
+    let requestId: number | undefined;
+
+    if (isActive()) {
+      let start: number | undefined;
+
+      function step(timestamp: number) {
+        if (start === undefined) {
+          start = timestamp;
+        }
+
+        const delta = timestamp - start;
+
+        setElapsedMs(delta + previousElapsedMs());
+        requestId = requestAnimationFrame(step);
+      }
+
+      requestId = requestAnimationFrame(step);
+    }
+
+    onCleanup(() => {
+      if (requestId !== undefined) {
+        cancelAnimationFrame(requestId);
+        requestId = undefined;
+      }
+    });
+  });
+
+  // TODO: implement this
+  function handleLapClick() {
+    //
+  }
+
+  // TODO: implement this
+  function handleStartClick() {
+    setIsActive(true);
+  }
+
+  // TODO: implement this
+  function handleStopClick() {
+    batch(() => {
+      setPreviousElapsedMs(elapsedMs());
+      setIsActive(false);
+    });
+  }
 
   return (
-    <div class="flex min-h-screen min-w-xs place-items-center">
-      <div class="mx-auto flex max-w-7xl flex-col space-y-10 p-8 text-center">
-        <div class="flex justify-center">
-          <a href="https://vite.dev">
-            <img
-              alt="Vite logo"
-              class="h-36 p-6 transition duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]"
-              src={viteLogo}
-            />
-          </a>
+    <div class="flex min-h-screen flex-col items-center justify-between p-8">
+      <p class="font-mono text-8xl">{formattedElapsedTime()}</p>
 
-          <a href="https://solidjs.com">
-            <img
-              alt="Solid logo"
-              class="h-36 p-6 transition duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa]"
-              src={solidLogo}
-            />
-          </a>
-        </div>
-
-        <h1 class="text-5xl font-semibold">Vite + Solid</h1>
-
-        <div class="flex flex-col items-center space-y-4 p-8">
-          <button
-            class="w-fit cursor-pointer rounded-lg border border-transparent bg-[#f9f9f9] px-5 py-2 font-medium transition duration-250 hover:border-[#646cff]"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is {count()}
-          </button>
-
-          <p class="font-light">
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-
-        <p class="text-neutral-400">
-          Click on the Vite and Solid logos to learn more
-        </p>
+      <div class="flex space-x-4">
+        {/* // TODO: leverage `<Show />` or `<Match />` for conditional rendering the various buttons */}
+        <Button onClick={handleLapClick}>Lap</Button>
+        <Show
+          when={isActive()}
+          fallback={<Button onClick={handleStartClick}>Start</Button>}
+        >
+          <Button onClick={handleStopClick}>Stop</Button>
+        </Show>
       </div>
     </div>
   );
-}
+};
 
 export default App;

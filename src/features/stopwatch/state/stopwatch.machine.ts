36beea72ceee61e46d6181cf70e6b1913431389f
Helgard Ferreira <hrfer3@gmail.com>
2025-10-01 23:10:05 +0200

    refactor: update stopwatch state machine state enum

--- src/features/stopwatch/state/stopwatch.machine.ts
+++ src/features/stopwatch/state/stopwatch.machine.ts
@@ -3,22 +3,22 @@ import type { ActorRefFrom, SnapshotFrom } from 'xstate';
 import { addLap, reset, updateSplit } from './actions';
 import { stopwatchMachineSetup } from './stopwatch.machine.setup';
 
-export enum StopwatchState {
+enum State {
   Active = 'Active',
   Idle = 'Idle',
   Stopped = 'Stopped',
 }
 
-export const stopwatchMachine = stopwatchMachineSetup.createMachine({
+const stopwatchMachine = stopwatchMachineSetup.createMachine({
   /** @xstate-layout N4IgpgJg5mDOIC5SwC4HsAOB3AhigxgBYB0AgvigJYBuYAxADY4YDaADALqKgZqyVU0AO24gAHogCsAdgCMxACwBmAGwAOJdOkAmFdICcagDQgAnom2yFxJbP0rJK7W0lt9hgL4eTqTLgIk5FS0dL6snKK8-IIiSOKISpL6ikoKavra0hoqsgYm5ghq8kra2ulsmYmyzkpePujYeERkFDT0AK4YEHhgAPqwGAwC7FxxUQKUwqISCJKS8tXKmjIZagrS+YiydsRsCmlqriUqqZp1IGH+zQCSEAz0qDgATigjkXwTU3EzidLE1ZpZJIFEC2LlJJsEKVrAoVGwlGw3GwivDVOdLk0SABlBoYSB0J5wMCvCJjD4xaZbJzEOZKJRrZHSBQySHOeTSRIODL0jkqdENK7Y3H4x4vN5k6KTWKgGayam0+kKRnMjZmBL7YhqDlg-RaFQ5MFebwgIRoCBwUQYgLvSVfGWIAC0KkhTt2iPdHvdVn5fkxLWCYBtn2l8QQsOsyMMCPSev0cshtmsSWUHIUmW0wIzPsaAWIt3uQYp3wsHMUKjS6jUezSclZ2msTjUGYqJTcVdqxqtzRxmDxEELUspYe0de0-zKze0rcM8KNHiAA */
   id: 'stopwatch',
   context: {
     currentSplit: 0,
     laps: [],
   },
-  initial: 'Idle',
+  initial: State.Idle,
   states: {
-    [StopwatchState.Active]: {
+    [State.Active]: {
       invoke: {
         id: 'currentSplit',
         input: ({ context }) => ({ previousSplit: context.currentSplit }),
@@ -29,23 +29,30 @@ export const stopwatchMachine = stopwatchMachineSetup.createMachine({
         lap: {
           actions: addLap,
           reenter: true,
-          target: 'Active',
+          target: State.Active,
         },
-        stop: 'Stopped',
+        stop: State.Stopped,
         update_split: { actions: updateSplit },
       },
     },
-    [StopwatchState.Idle]: {
-      on: { start: 'Active' },
+    [State.Idle]: {
+      on: { start: State.Active },
     },
-    [StopwatchState.Stopped]: {
+    [State.Stopped]: {
       on: {
-        reset: { actions: reset, target: 'Idle' },
-        start: 'Active',
+        reset: { actions: reset, target: State.Idle },
+        start: State.Active,
       },
     },
   },
 });
 
-export type StopwatchActorRef = ActorRefFrom<typeof stopwatchMachine>;
-export type StopwatchActorSnapshot = SnapshotFrom<typeof stopwatchMachine>;
+type StopwatchActorRef = ActorRefFrom<typeof stopwatchMachine>;
+type StopwatchActorSnapshot = SnapshotFrom<typeof stopwatchMachine>;
+
+export {
+  State as StopwatchState,
+  type StopwatchActorRef,
+  type StopwatchActorSnapshot,
+  stopwatchMachine,
+};
